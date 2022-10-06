bb9ac50845a5e292444557c259471cce9b412613
emptycan1010 <85441644+emptycan1010@users.noreply.github.com>
2022-10-06 22:38:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="novelai-img-lib",
-    version="0.1.6",
+    version="0.1.7",
     description="NovelAI-Image Library for python ",
     author="emptycan1010",
     author_email="siro157@duck.com",
